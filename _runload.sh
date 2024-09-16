2002abf3a61753bc90e6af2a50bbb55f7407769f
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2024-09-16 15:13:09 -0400

    fix scripts

--- _runload.sh
+++ _runload.sh
@@ -5,6 +5,11 @@
 export HEADER="Content-Type: application/fhir+json"
 export FHIR="http://localhost:8080/fhir"
 
+bash _refresh.sh
+
+# bash _genonce.sh
+java -Xmx4g -jar ./input-cache/publisher.jar publisher -ig .
+
 # add patient examples
 
 for FILE in input/test-transforms/*.json ; do curl -X POST -H "$HEADER" --data @${FILE} $FHIR | jq . ; done
@@ -18,8 +23,5 @@ printf "Medication Statements: ${ms}\n"
 con=$(curl -s http://localhost:8080/fhir/Condition?_total=accurate | jq .total)
 printf "Conditions: ${con}\n"
 
+curl --show-error --write-out '%{http_code}' --output /dev/null -s -X PUT -H "$HEADER" --data @Library-FHIR-ModelInfo.json ${FHIR}/Library/FHIR-ModelInfo
 
-bash _refresh.sh
-
-# bash _genonce.sh
-java -Xmx4g -jar ./input-cache/publisher.jar publisher -ig .
--- _runmeasures.sh
+++ _runmeasures.sh
@@ -5,17 +5,22 @@
 export HEADER="Content-Type: application/fhir+json"
 export FHIR="http://localhost:8080/fhir"
 
-curl -s -X PUT -H "$HEADER" --data @Library-FHIR-ModelInfo.json $FHIR/Library/FHIR-ModelInfo | jq .
-
 MEASURES=(HIVIND18Logic HIVIND27Logic HIVIND28Logic HIVIND29Logic HIVIND30Logic)
+MSHORT=(HIVIND18 HIVIND27 HIVIND28 HIVIND29 HIVIND30)
 LIBRARIES=(FHIRHelpers FHIRCommon HIVCommon HIVConfig HIVConcepts HIVConceptsCustom HIVElements HIVEncounterElements)
 
-PERSTART=(2021-01-01 2022-01-01)
-PERSTOP=(2021-12-31 2022-12-31)
+# PERSTART=(2021-01-01 2022-01-01)
+# PERSTOP=(2021-12-31 2022-12-31)
 
+
+# bundles/measure/Measure-HIVIND18/Measure-HIVIND18-bundle.json
+
+# for FILE in ${MSHORT[*]}; do echo "bundles/measure/Measure-${FILE}/Measure-${FILE}-bundle.json"; done
+
+for FILE in ${MSHORT[*]}; do curl -sS -d @bundles/measure/Measure-${FILE}/Measure-${FILE}-bundle.json -H "$HEADER" -X POST ${FHIR} | jq .; done
 
 # TEST
-for FILE in ${LIBRARIES[*]} ${MEASURES[*]}; do echo "${FILE}"; done
+# for FILE in ${LIBRARIES[*]} ${MEASURES[*]}; do echo "${FILE}"; done
 
 # libaries
 # for FILE in ${LIBRARIES[*]}; do curl -X PUT -H "$HEADER" --data @output/Library-${FILE}.json $FHIR/Library/${FILE} | jq . ; done
@@ -23,21 +28,21 @@ for FILE in ${LIBRARIES[*]} ${MEASURES[*]}; do echo "${FILE}"; done
 # measures
 # for FILE in ${MEASURES[*]}; do curl -X PUT -H "$HEADER" --data @output/Measure-${FILE}.json $FHIR/Measure/${FILE} | jq . ; done
 
-# run measures
-# for FILE in ${MEASURES[*]}; do curl $FHIR'/Measure/{FILE}/$evaluate-measure?periodStart=2000-01-01&periodEnd=2021-12-31' | jq . ; done
+# run test measures
+for FILE in ${MSHORT[*]}; do curl "${FHIR}/Measure/${FILE}/%24evaluate-measure?periodStart=2023-12-01&periodEnd=2023-12-31" | jq . > measure-results/${FILE}-2023-12.json; done
 
-printf "\nPeriods evaluated\n"
+# printf "\nPeriods evaluated\n"
 
-for FILE in ${MEASURES[*]}
-do
-    for i in ${PERSTART}
-    do
-        for j in ${PERSTOP}
-        do 
-            echo "${FILE}: $i to $j"
-        done
-    done
-done
+# for FILE in ${MEASURES[*]}
+# do
+#     for i in ${PERSTART}
+#     do
+#         for j in ${PERSTOP}
+#         do 
+#             echo "${FILE}: $i to $j"
+#         done
+#     done
+# done
 
 
 
