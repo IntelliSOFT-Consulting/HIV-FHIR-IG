c250184af9a722bb20451529eb14ac4910d4fc86
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2024-09-17 11:46:20 -0400

    fixes, update cqf tooling

--- _runload.sh
+++ _runload.sh
@@ -5,14 +5,18 @@
 export HEADER="Content-Type: application/fhir+json"
 export FHIR="http://localhost:8080/fhir"
 
-bash _refresh.sh
+# bash _refresh.sh
 
 # bash _genonce.sh
-java -Xmx4g -jar ./input-cache/publisher.jar publisher -ig .
+# java -Xmx4g -jar ./input-cache/publisher.jar publisher -ig .
 
 # add patient examples
+curl -X POST -H "$HEADER" --data @input/test-transforms/0a2f4c9b-0963-4ea6-8d8c-da9b461460d1.json $FHIR | jq .
+curl -X POST -H "$HEADER" --data @input/test-transforms/0be33662-350d-460f-bea2-bf2c13219c60.json $FHIR | jq .
 
-for FILE in input/test-transforms/*.json ; do curl -X POST -H "$HEADER" --data @${FILE} $FHIR | jq . ; done
+
+
+# for FILE in input/test-transforms/*.json ; do curl -X POST -H "$HEADER" --data @${FILE} $FHIR | jq . ; done
 
 pat=$(curl -s http://localhost:8080/fhir/Patient?_total=accurate | jq .total)
 printf "Patients: ${pat}\n"
